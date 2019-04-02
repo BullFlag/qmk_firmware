b92a55e231f35cd345543b046fed3c09412eb747
John Liebenrood <john@liebenrood.net>
2019-04-01 19:45:39 -0700

    LEADER Initial Commit
     * Using Leader Key assigned to Caps Lock
     Add Support to Silent Hunter

---
+++ keyboards/massdrop/ctrl/keymaps/sh/keymap.c
@@ -0,0 +1,342 @@
+/*
+
+JDL Monday April 1 2019
+Changes to add Silent Hunter Support
+  Assign Leader Key with [ ] \ to Time compression
+
+Changes to Default keymap
+  Moved 2 CTRL LED key assignments Tab is now R and Capslock is now C
+  Put Leader key on layer 0 made sure it's transparent on upper layers
+  Leader key assigned to Cap Lock
+  Copied config.h and rules.mk from root of CTRL then added Leader key code at EOF
+  Added in Support for Evernote with Keyboard Maestro remapping for flexibility
+  Added in Support for macOS Screen shot
+  Added in Alert to flash LED backlighting when no Sequence matched, failure
+  Added in Support for go to Sleep on Leader Pause ( F15 )
+
+LEADER KEY
+Leader Key described here https://thomasbaart.nl/2018/12/20/qmk-basics-leader-key/
+Official doc https://docs.qmk.fm/#/feature_leader_key
+
+Leader Key, keycode KC_LEAD is using sequences for shortcuts.  It allows you to define
+sequences with which you can trigger keypresses and functions, making complicated
+shortcuts easier, and can make the shortcuts easier to remember to boot.
+
+
+*/
+#include QMK_KEYBOARD_H
+
+enum ctrl_keycodes {
+    L_BRI = SAFE_RANGE, //LED Brightness Increase
+    L_BRD,              //LED Brightness Decrease
+    L_PTN,              //LED Pattern Select Next
+    L_PTP,              //LED Pattern Select Previous
+    L_PSI,              //LED Pattern Speed Increase
+    L_PSD,              //LED Pattern Speed Decrease
+    L_T_MD,             //LED Toggle Mode
+    L_T_ONF,            //LED Toggle On / Off
+    L_ON,               //LED On
+    L_OFF,              //LED Off
+    L_T_BR,             //LED Toggle Breath Effect
+    L_T_PTD,            //LED Toggle Scrolling Pattern Direction
+    U_T_AUTO,           //USB Extra Port Toggle Auto Detect / Always Active
+    U_T_AGCR,           //USB Toggle Automatic GCR control
+    DBG_TOG,            //DEBUG Toggle On / Off
+    DBG_MTRX,           //DEBUG Toggle Matrix Prints
+    DBG_KBD,            //DEBUG Toggle Keyboard Prints
+    DBG_MOU,            //DEBUG Toggle Mouse Prints
+    MD_BOOT,            //Restart into bootloader after hold timeout
+};
+
+#define TG_NKRO MAGIC_TOGGLE_NKRO //Toggle 6KRO / NKRO mode
+
+keymap_config_t keymap_config;
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,             KC_PSCR, KC_SLCK, KC_PAUS, \
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,   KC_INS,  KC_HOME, KC_PGUP, \
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,   KC_DEL,  KC_END,  KC_PGDN, \
+        KC_LEAD, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT, \
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                              KC_UP, \
+        KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, MO(1),   KC_APP,  KC_RCTL,            KC_LEFT, KC_DOWN, KC_RGHT \
+    ),
+    [1] = LAYOUT(
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,            KC_MUTE, KC_TRNS, KC_TRNS, \
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_MPLY, KC_MSTP, KC_VOLU, \
+        KC_TRNS, L_PSD,   L_BRI,   L_PSI,   L_T_BR, KC_TRNS, KC_TRNS, U_T_AUTO,U_T_AGCR,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_MPRV, KC_MNXT, KC_VOLD, \
+        KC_TRNS, L_PTP,   L_BRD,   L_PTN,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+        KC_TRNS, L_T_MD,  L_T_ONF, L_T_PTD, KC_TRNS, MD_BOOT, TG_NKRO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                              KC_TRNS, \
+        KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,            KC_TRNS, KC_TRNS, KC_TRNS \
+    ),
+
+    /*
+    [X] = LAYOUT(
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,            KC_TRNS, KC_TRNS, KC_TRNS, \
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, \
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, \
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TG_NKRO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                              KC_TRNS, \
+        KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,            KC_TRNS, KC_TRNS, KC_TRNS \
+    ),
+    */
+};
+
+const uint16_t PROGMEM fn_actions[] = {
+
+};
+
+
+// Runs just one time when the keyboard initializes.
+void matrix_init_user(void) {
+};
+
+// Runs constantly in the background, in a loop.
+
+// JDL Added to support Leader key feature
+LEADER_EXTERNS();
+
+// Declare a boolean variable to keep track of whether any sequence
+// will have been matched.
+bool did_leader_succeed;
+
+void matrix_scan_user(void) {
+  LEADER_DICTIONARY() {
+    // Initialize did_leader_succeed as well as leading to be false
+    did_leader_succeed = leading = false;
+
+
+    // Replace the sequences below with your own sequences.
+
+
+
+    SEQ_ONE_KEY(KC_A) {
+      // When I press KC_LEAD and then A, this sends CTRL + SHIFT + A Launch Keyboard Maestro
+      SEND_STRING(SS_LCTRL(SS_LSFT("a")));
+      // In each sequence, set our flag to true. This way, we'll
+      // know when any sequence was matched.
+      did_leader_succeed = true;
+    }
+
+    // These next four assignments are remapped in Keyboard Maestro for Evernote
+    SEQ_ONE_KEY(KC_W) {
+      // When I press KC_LEAD and then W, this sends CTRL + SHIFT + W Increase Indent
+      SEND_STRING(SS_LCTRL(SS_LSFT("w")));
+      did_leader_succeed = true;
+    }
+    SEQ_ONE_KEY(KC_Q) {
+      // When I press KC_LEAD and then Q, this sends CTRL + SHIFT + Q Decrease Indent
+      SEND_STRING(SS_LCTRL(SS_LSFT("q")));
+      did_leader_succeed = true;
+
+    }
+    SEQ_ONE_KEY(KC_X) {
+      // When I press KC_LEAD and then X, this sends CTRL + SHIFT + X Clear Style
+      SEND_STRING(SS_LCTRL(SS_LSFT("x")));
+      did_leader_succeed = true;
+    }
+    SEQ_ONE_KEY(KC_Z) {
+      // When I press KC_LEAD and then Z, this sends CTRL + SHIFT + Z Title 18pt font
+      SEND_STRING(SS_LCTRL(SS_LSFT("z")));
+      did_leader_succeed = true;
+    }
+
+    SEQ_ONE_KEY(KC_S) {
+      // When I press KC_LEAD and then S, this sends CMD + SHIFT + 5 macOS ScrShot
+      SEND_STRING(SS_LALT(SS_LSFT("5")));
+      did_leader_succeed = true;
+    }
+
+    SEQ_ONE_KEY(KC_GRAVE) {
+      // When I press KC_LEAD and then `, this sends CTRL + SHIFT + ` OnePassW
+      SEND_STRING(SS_LCTRL(SS_LSFT("`")));
+      did_leader_succeed = true;
+    }
+
+    SEQ_ONE_KEY(KC_F12) {
+      // When I press KC_LEAD and then Pause, this sends CTRL + SHIFT + F12  Sleep
+      SEND_STRING(SS_LCTRL(SS_LSFT("F12")));
+      did_leader_succeed = true;
+    }
+
+
+
+    // Add Silent Hunter Time Compression support
+
+
+    SEQ_ONE_KEY(KC_LBRACKET) {
+      // When I Press KC_LEAD And Then [ , This Sends Numpad Plus  Inc Compression
+      SEND_STRING(KC_KP_PLUS);
+      did_leader_succeed = true;
+    }
+
+    SEQ_ONE_KEY(KC_RBRACKET) {
+     // When I Press KC_LEAD And Then ] , This Sends Shift + Numpad Plus  Compression=Max
+      SEND_STRING(SS_LSFT(KC_KP_PLUS));
+      did_leader_succeed = true;
+    }
+
+     SEQ_ONE_KEY((KC_BSLASH) {
+      // When I Press KC_LEAD And Then \ , This Sends Shift + Numpad Minus  Compression=1
+      SEND_STRING(SS_LSFT(KC_KP_MINUS)) ;
+      did_leader_succeed = true;
+    }
+
+
+
+
+    // Note: This is not an array, you don't need to put any commas
+    // or semicolons between sequences.
+
+
+    SEQ_TWO_KEYS(KC_K, KC_M) {
+      // When I press KC_LEAD and then K followed by M, this sends CTRL + SHIFT + A
+      SEND_STRING(SS_LCTRL(SS_LSFT("a")));
+      did_leader_succeed = true;
+    }
+
+    // Call leader_end at the end of the function, instead of at
+    // the start. This way, we're sure we have set did_leader_succeed.
+    leader_end();
+  }
+}
+
+void leader_end(void) {
+  if (did_leader_succeed) {
+    // If any sequence was matched, did_leader_succeed will have
+    // been set to true up in the matrix_scan_user function.
+    // Put your code for a matched leader key sequence here.
+  } else {
+    // If no sequence was matched, did_leader_succeed will not
+    // have been set to true anywhere, so we'll end up here.
+    // Put your code for an unmatched leader key sequence here.
+  }
+}
+
+#define MODS_SHIFT  (keyboard_report->mods & MOD_BIT(KC_LSHIFT) || keyboard_report->mods & MOD_BIT(KC_RSHIFT))
+#define MODS_CTRL  (keyboard_report->mods & MOD_BIT(KC_LCTL) || keyboard_report->mods & MOD_BIT(KC_RCTRL))
+#define MODS_ALT  (keyboard_report->mods & MOD_BIT(KC_LALT) || keyboard_report->mods & MOD_BIT(KC_RALT))
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    static uint32_t key_timer;
+
+    switch (keycode) {
+        case L_BRI:
+            if (record->event.pressed) {
+                if (LED_GCR_STEP > LED_GCR_MAX - gcr_desired) gcr_desired = LED_GCR_MAX;
+                else gcr_desired += LED_GCR_STEP;
+                if (led_animation_breathing) gcr_breathe = gcr_desired;
+            }
+            return false;
+        case L_BRD:
+            if (record->event.pressed) {
+                if (LED_GCR_STEP > gcr_desired) gcr_desired = 0;
+                else gcr_desired -= LED_GCR_STEP;
+                if (led_animation_breathing) gcr_breathe = gcr_desired;
+            }
+            return false;
+        case L_PTN:
+            if (record->event.pressed) {
+                if (led_animation_id == led_setups_count - 1) led_animation_id = 0;
+                else led_animation_id++;
+            }
+            return false;
+        case L_PTP:
+            if (record->event.pressed) {
+                if (led_animation_id == 0) led_animation_id = led_setups_count - 1;
+                else led_animation_id--;
+            }
+            return false;
+        case L_PSI:
+            if (record->event.pressed) {
+                led_animation_speed += ANIMATION_SPEED_STEP;
+            }
+            return false;
+        case L_PSD:
+            if (record->event.pressed) {
+                led_animation_speed -= ANIMATION_SPEED_STEP;
+                if (led_animation_speed < 0) led_animation_speed = 0;
+            }
+            return false;
+        case L_T_MD:
+            if (record->event.pressed) {
+                led_lighting_mode++;
+                if (led_lighting_mode > LED_MODE_MAX_INDEX) led_lighting_mode = LED_MODE_NORMAL;
+            }
+            return false;
+        case L_T_ONF:
+            if (record->event.pressed) {
+                led_enabled = !led_enabled;
+                I2C3733_Control_Set(led_enabled);
+            }
+            return false;
+        case L_ON:
+            if (record->event.pressed) {
+                led_enabled = 1;
+                I2C3733_Control_Set(led_enabled);
+            }
+            return false;
+        case L_OFF:
+            if (record->event.pressed) {
+                led_enabled = 0;
+                I2C3733_Control_Set(led_enabled);
+            }
+            return false;
+        case L_T_BR:
+            if (record->event.pressed) {
+                led_animation_breathing = !led_animation_breathing;
+                if (led_animation_breathing) {
+                    gcr_breathe = gcr_desired;
+                    led_animation_breathe_cur = BREATHE_MIN_STEP;
+                    breathe_dir = 1;
+                }
+            }
+            return false;
+        case L_T_PTD:
+            if (record->event.pressed) {
+                led_animation_direction = !led_animation_direction;
+            }
+            return false;
+        case U_T_AUTO:
+            if (record->event.pressed && MODS_SHIFT && MODS_CTRL) {
+                TOGGLE_FLAG_AND_PRINT(usb_extra_manual, "USB extra port manual mode");
+            }
+            return false;
+        case U_T_AGCR:
+            if (record->event.pressed && MODS_SHIFT && MODS_CTRL) {
+                TOGGLE_FLAG_AND_PRINT(usb_gcr_auto, "USB GCR auto mode");
+            }
+            return false;
+        case DBG_TOG:
+            if (record->event.pressed) {
+                TOGGLE_FLAG_AND_PRINT(debug_enable, "Debug mode");
+            }
+            return false;
+        case DBG_MTRX:
+            if (record->event.pressed) {
+                TOGGLE_FLAG_AND_PRINT(debug_matrix, "Debug matrix");
+            }
+            return false;
+        case DBG_KBD:
+            if (record->event.pressed) {
+                TOGGLE_FLAG_AND_PRINT(debug_keyboard, "Debug keyboard");
+            }
+            return false;
+        case DBG_MOU:
+            if (record->event.pressed) {
+                TOGGLE_FLAG_AND_PRINT(debug_mouse, "Debug mouse");
+            }
+            return false;
+        case MD_BOOT:
+            if (record->event.pressed) {
+                key_timer = timer_read32();
+            } else {
+                if (timer_elapsed32(key_timer) >= 500) {
+                    reset_keyboard();
+                }
+            }
+            return false;
+        default:
+            return true; //Process all other keycodes normally
+    }
+}
